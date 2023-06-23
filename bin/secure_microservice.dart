21d2a654a07b8fad487dec0b8f0e251fbf04a00e
geneyllanes <gene.yllanes@americansystems.com>
2023-06-23 10:53:21 -0400

    Initial commit

---
+++ bin/secure_microservice.dart
@@ -0,0 +1,5 @@
+import 'package:secure_microservice/secure_microservice.dart' as secure_microservice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${secure_microservice.calculate()}!');
+}
---
+++ test/secure_microservice_test.dart
@@ -0,0 +1,8 @@
+import 'package:secure_microservice/secure_microservice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
